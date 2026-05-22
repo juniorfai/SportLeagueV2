53c592b821a0efbe66d02d4f5f18303bbc2a4910
juniorfai <107644136+juniorfai@users.noreply.github.com>
2026-05-22 18:48:18 -0500

    Phase 4 completed

---
+++ SportsLeague.API/Mappings/MappingProfile.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using SportsLeague.API.DTOs.Request;
+using SportsLeague.API.DTOs.Response;
+using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.API.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Team mappings
+            CreateMap<TeamRequestDTO, Team>();
+            CreateMap<Team, TeamResponseDTO>();
+
+            // Player mappings
+            CreateMap<PlayerRequestDTO, Player>();
+            CreateMap<Player, PlayerResponseDTO>()
+                .ForMember(
+                    dest => dest.TeamName,
+                    opt => opt.MapFrom(src => src.Team.Name));
+
+            // Referee mappings
+            CreateMap<RefereeRequestDTO, Referee>();
+            CreateMap<Referee, RefereeResponseDTO>();
+
+            // Tournament mappings
+            CreateMap<TournamentRequestDTO, Tournament>();
+            CreateMap<Tournament, TournamentResponseDTO>()
+                .ForMember(
+                    dest => dest.TeamsCount,
+                    opt => opt.MapFrom(src =>
+                        src.TournamentTeams != null ? src.TournamentTeams.Count : 0)); //Condición ternaria
+
+            // Match mappings
+            CreateMap<MatchRequestDTO, Match>();
+            CreateMap<Match, MatchResponseDTO>()
+                .ForMember(dest => dest.TournamentName,
+                    opt => opt.MapFrom(src => src.Tournament.Name))
+                .ForMember(dest => dest.HomeTeamName,
+                    opt => opt.MapFrom(src => src.HomeTeam.Name))
+                .ForMember(dest => dest.AwayTeamName,
+                    opt => opt.MapFrom(src => src.AwayTeam.Name))
+                .ForMember(dest => dest.RefereeFullName,
+                    opt => opt.MapFrom(src =>
+                        src.Referee.FirstName + " " + src.Referee.LastName));
+        }
+    }
+}
---
+++ SportsLeague.Domain/Entities/Team.cs
@@ -0,0 +1,21 @@
+﻿namespace SportsLeague.Domain.Entities
+{
+    public class Team : AuditBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Stadium { get; set; } = string.Empty;
+        public string? LogoUrl { get; set; }
+        public DateTime FoundedDate { get; set; }
+
+        // Navigation Property - Colección de jugadores
+        public ICollection<Player> Players { get; set; } = new List<Player>();
+        public ICollection<TournamentTeam> TournamentTeams { get; set; } = new List<TournamentTeam>();
+        
+        // Partidos como local
+        public ICollection<Match> HomeMatches { get; set; } = new List<Match>();
+        // Partidos como visitante
+        public ICollection<Match> AwayMatches { get; set; } = new List<Match>();
+
+    }
+}
